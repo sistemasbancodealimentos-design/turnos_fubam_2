9b1d71207129c9c84c6199384308f163594ec9ee
sistemasbancodealimentos-design <sistemasbancodealimentos@gmail.com>
2026-03-25 07:09:13 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs      = require('fs');
 const path    = require('path');
 
 const app     = express();
-const PORT    = 3000;
+const PORT    = process.env.PORT || 3000;
 const DB_FILE = path.join(__dirname, 'turnos.json');
 
 app.use(cors());
